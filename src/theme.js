6522175d285cdfaaf39ee774571e1236478d8a35
Ikramuddin21 <shaonakhand2122@gmail.com>
2023-11-01 13:49:36 +0600

    added material ui

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FF0000",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+  },
+});
+
+export default theme;
